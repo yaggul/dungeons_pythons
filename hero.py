edc27bf9571ae1eaca8af63f5a6abd345ef02aec
Nikolay <nidokuzov@gmail.com>
2018-04-23 19:53:09 +0300

    constructor change

--- hero.py
+++ hero.py
@@ -1,8 +1,7 @@
 class Hero:
-    def __init__(self, name, title, health, mana, regeneration_rate):
+    def __init__(self, name= None, title= None, health= 0, mana= 0, regeneration_rate= 0):
         self.name = name
         self.title = title
         self.health = health
         self.mana = mana
         self.regeneration_rate = regeneration_rate
-    

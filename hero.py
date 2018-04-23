c357a3e0f9f5a514be1c7d239d39ed478a3edb94
Nikolay <nidokuzov@gmail.com>
2018-04-23 19:47:17 +0300

    adding a construction

---
+++ hero.py
@@ -0,0 +1,8 @@
+class Hero:
+    def __init__(self, name, title, health, mana, regeneration_rate):
+        self.name = name
+        self.title = title
+        self.health = health
+        self.mana = mana
+        self.regeneration_rate = regeneration_rate
+    

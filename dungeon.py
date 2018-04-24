9d74436508b23779199e1111ea8dcf2e8fe82e7f
Boyan Bonev <boqn_bonev@abv.bg>
2018-04-24 11:02:02 +0300

    fixed dungeon

--- dungeon.py
+++ dungeon.py
@@ -1,10 +1,18 @@
-def Dungeon(filename):
-    with open(filename, 'r') as f:
-        matrix = []
-        lines = f.readlines()
-        for line in lines:
-            row = []
-            for i in line:
-                row.append(i)
-            matrix.append(row[:-1])
-        return matrix
+
+class Dungeon:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def print_map(self):
+        with open(self.filename, 'r') as f:
+            matrix = []
+            lines = f.readlines()
+            for line in lines:
+                row = []
+                for i in line:
+                    row.append(i)
+                matrix.append(row[:-1])
+            for row in matrix:
+                for coll in row:
+                    print(coll, end='')
+                print()

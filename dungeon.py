ab1d764fa5abe8402572e88c38ce4a23ac5ac2f1
Boyan Bonev <boqn_bonev@abv.bg>
2018-04-25 11:29:45 +0300

    added move_hero without cases

--- dungeon.py
+++ dungeon.py
@@ -1,18 +1,84 @@
+from hero import Hero
+
 
 class Dungeon:
-    def __init__(self, filename):
-        self.filename = filename
 
-    def print_map(self):
+    def __init__(self, filename, hero=None):
+        self.filename = filename
+        self.matrix = []
+        self.rows = 0
+        self.cols = 0
+
+    def open_file(self):
         with open(self.filename, 'r') as f:
-            matrix = []
             lines = f.readlines()
             for line in lines:
                 row = []
                 for i in line:
                     row.append(i)
-                matrix.append(row[:-1])
-            for row in matrix:
-                for coll in row:
-                    print(coll, end='')
+                self.matrix.append(row[:-1])
+        return self.matrix
+
+    def print_map(self):
+        if len(self.matrix) == 0:
+            self.open_file()
+            for row in self.matrix:
+                for col in row:
+                    print(col, end='')
                 print()
+        else:
+            for row in self.matrix:
+                for col in row:
+                    print(col, end='')
+                print()
+
+    def spawn(self, Hero):
+        self.rows = len(self.matrix)
+        self.cols = len(self.matrix[0])
+
+        for i in range(self.rows):
+            for j in range(self.cols):
+                if self.matrix[i][j] == 'S':
+                    self.matrix[i][j] = 'H'
+
+    def valid_position(self, row, col):
+        if row < 0 or row >= self.rows or col < 0 or col >= self.cols:
+            return False
+        else:
+            return True
+
+    def move_hero(self, direction):
+        rows = len(self.matrix)
+        cols = len(self.matrix[0])
+
+        for i in range(rows):
+            for j in range(cols):
+                if self.matrix[i][j] == 'H':
+                    if direction == 'left':
+                        if self.valid_position(i, j -1):
+                            self.matrix[i][j - 1] = 'H'
+                            self.matrix[i][j] = '.'
+                            return True
+                        else:
+                            return False
+                    elif direction == 'down':
+                        if self.valid_position(i + 1, j):
+                            self.matrix[i + 1][j] = 'H'
+                            self.matrix[i][j] = '.'
+                            return True
+                        else:
+                            return False
+                    if direction == 'up':
+                        if self.valid_position(i - 1, j):
+                            self.matrix[i - 1][j] = 'H'
+                            self.matrix[i][j] = '.'
+                            return True
+                        else:
+                            return False
+                    elif direction == 'right':
+                        if self.valid_position(i, j + 1):
+                            self.matrix[i][j + 1] = 'H'
+                            self.matrix[i][j] = '.'
+                            return True
+                        else:
+                            return False
--- map.py
+++ map.py
@@ -1,5 +1,26 @@
 from dungeon import Dungeon
+from hero import Hero
+
+h = Hero(name="Bron", title="Dragonslayer", health=100, mana=100, mana_regeneration_rate=2)
 
 map = Dungeon('level1.txt')
 
+map.print_map()
+print()
+map.spawn(h)
+
+print()
+map.print_map()
+map.move_hero('right')
+print('Move right')
 print(map.print_map())
+print('Move left')
+map.move_hero('left')
+map.print_map()
+print('Move down')
+map.move_hero('down')
+map.print_map()
+print('Move up')
+map.move_hero('up')
+map.print_map()
+
